9c1236221b9c8383496f5b060b4b32487f3278c2
kw22228 <hot5521@naver.com>
2021-11-04 17:11:28 +0900

    App 수정
    Components 추가

--- beginner/src/App.js
+++ beginner/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Composition from './components/2-4Props/Composition';
 import Extraction from './components/2-4Props/Extraction/Extraction';
-
+import ClassComponents from './components/2-5State/ClassComponent';
+import FunctionalComponent from './components/2-5State/FunctionalComponent'
 function App() {
   return (
     <div className="App">
+      <FunctionalComponent />
+      <ClassComponents />
       <Extraction />
       <Composition />
     </div>
