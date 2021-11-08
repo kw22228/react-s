1850ad1973ffe06d7f63e0f63df0e213181d443c
kw22228 <hot5521@naver.com>
2021-11-08 14:50:29 +0900

    수정

--- beginner/src/App.js
+++ beginner/src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import Composition from './components/2-4Props/Composition';
 import Extraction from './components/2-4Props/Extraction/Extraction';
 import ClassComponents from './components/2-5State/ClassComponent';
-import FunctionalComponent from './components/2-5State/FunctionalComponent'
+import { FunctionalComponent } from './components/2-5State/FunctionalComponent'
+import ClassComponent from './components/2-6LifeCycle/ClassComponent';
 function App() {
   return (
     <div className="App">
-      <FunctionalComponent />
+      <ClassComponent />
+      {/* <FunctionalComponent />
       <ClassComponents />
       <Extraction />
-      <Composition />
+      <Composition /> */}
     </div>
   );
 }
