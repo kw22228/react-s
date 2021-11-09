f841498f08ebb06eab3ee0ec35c8d3fefae5464a
kw22228 <hot5521@naver.com>
2021-11-09 16:53:13 +0900

    수정

--- beginner/src/App.js
+++ beginner/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import Composition from './components/2-4Props/Composition';
-import Extraction from './components/2-4Props/Extraction/Extraction';
-import ClassComponents from './components/2-5State/ClassComponent';
-import { FunctionalComponent } from './components/2-5State/FunctionalComponent'
-import ClassComponent from './components/2-6LifeCycle/ClassComponent';
-import Event from './components/2-7Event/Event';
-import Condition from './components/2-8ConditionalRendering/Condition';
+// import Composition from './components/2-4Props/Composition';
+// import Extraction from './components/2-4Props/Extraction/Extraction';
+// import ClassComponents from './components/2-5State/ClassComponent';
+// import { FunctionalComponent } from './components/2-5State/FunctionalComponent'
+// import ClassComponent from './components/2-6LifeCycle/ClassComponent';
+// import Event from './components/2-7Event/Event';
+// import Condition from './components/2-8ConditionalRendering/Condition';
+import List from './components/2-9List/List';
+
 function App() {
   return (
     <div className="App">
-      <Condition />
+      <List />
+      {/* <Condition /> */}
       {/* <Event /> */}
       {/* <ClassComponent /> */}
       {/* <FunctionalComponent />
