e28dad7e5fc479832cd21b47990d3099d51cae99
kw22228 <hot5521@naver.com>
2021-11-14 19:45:52 +0900

    Composition 1.

---
+++ advanced/src/components/Composition/WelcomeDialog.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import CustomDialog from './CustomDialog';
+// import Dialog from './Dialog';
+
+export default function WelcomeDialog() {
+    return (
+        // <Dialog>
+        //     <h1>Welcome.</h1>
+        //     <h5>Thank you!</h5>
+        // </Dialog>
+        <CustomDialog
+            title={<button>Welcome</button>}
+            description="Thank you!"
+        />
+    );
+}
