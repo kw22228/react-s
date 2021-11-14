d1ae86288a625435440c97a8a99986296cd5b81d
kw22228 <hot5521@naver.com>
2021-11-14 19:44:11 +0900

    reducer 실습

--- advanced/src/App.js
+++ advanced/src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
-import State from './components/Hooks/State';
-import Reducer from './components/Hooks/Reducer'
+import Button from './components/HOC/Button';
+import Input from './components/HOC/Input';
+// import ThankyouDialog from './components/Composition2/ThankyouDialog';
+// import State from './components/Hooks/State';
+// import Reducer from './components/Hooks/Reducer';
+// import WelcomeDialog from './components/Composition/WelcomeDialog';
+// import Dialog from './components/Composition2/Dialog';
 
 function App() {
-  return (
-    <div className="App">
-      <Reducer />
-      <State />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Input />
+            <br />
+            <br />
+            <Button />
+            {/* <Reducer />
+            <State /> */}
+            {/* <WelcomeDialog /> */}
+            {/* <ThankyouDialog /> */}
+        </div>
+    );
 }
 
 export default App;
--- advanced/src/components/Hooks/Reducer.jsx
+++ advanced/src/components/Hooks/Reducer.jsx
@@ -1,16 +1,16 @@
-import React, { useReducer } from 'react'
+import React, { useReducer } from 'react';
 
 export default function Reducer() {
-    const initialState = {count: 0, type: 'Loading...'};
+    const initialState = { count: 0, type: 'Loading...' };
 
-    function reducer(state, action){
-        switch(action.type){
+    function reducer(state, action) {
+        switch (action.type) {
             case 'reset':
                 return initialState;
             case 'increase':
-                return {count: state.count + 1, type: 'increase'};
+                return { count: state.count + 1, type: 'increase' };
             case 'decrease':
-                return {count: state.count - 1, type: 'decrease'};
+                return { count: state.count - 1, type: 'decrease' };
             default:
                 throw new Error();
         }
@@ -20,12 +20,15 @@ export default function Reducer() {
 
     return (
         <div>
-            Reducer Count : {state.count}<br />
-            {state.type}<br/>
-            <button onClick={() => dispatch({type: 'reset'})}>Reset</button><br />
-            <button onClick={() => dispatch({type: 'decrease'})}>-</button><br />
-            <button onClick={() => dispatch({type: 'increase'})}>+</button>
-            
+            Reducer Count : {state.count}
+            <br />
+            {state.type}
+            <br />
+            <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+            <br />
+            <button onClick={() => dispatch({ type: 'decrease' })}>-</button>
+            <br />
+            <button onClick={() => dispatch({ type: 'increase' })}>+</button>
         </div>
-    )
+    );
 }
