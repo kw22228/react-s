c9d47a8bcb765900eec7ab27b5b94fbdd6016df3
kw22228 <hot5521@naver.com>
2021-11-14 19:46:06 +0900

    composition2

---
+++ advanced/src/components/Composition2/Dialog.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+export default function Dialog(props) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+            <button onClick={() => setIsOpen(true)}>Open</button>
+            {isOpen && (
+                <div
+                    style={{
+                        position: 'absolute',
+                        zIndex: 99,
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        border: '1px solid black',
+                        padding: 24,
+                        backgroundColor: 'white',
+                    }}
+                >
+                    {typeof props.title === 'string' ? (
+                        <h1>{props.title}</h1>
+                    ) : (
+                        props.title
+                    )}
+                    {typeof props.description === 'string' ? (
+                        <h5>{props.description}</h5>
+                    ) : (
+                        props.description
+                    )}
+                    {typeof props.button === 'string' ? (
+                        <button
+                            style={{ backgroundColor: 'red', color: 'white' }}
+                            onClick={() => setIsOpen(false)}
+                        >
+                            {props.button}
+                        </button>
+                    ) : (
+                        //이벤트 버블링을 이용한다.
+                        <div onClick={() => setIsOpen(false)}>
+                            {props.button}
+                        </div>
+                    )}
+                </div>
+            )}
+            {isOpen && (
+                <div
+                    style={{
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        bottom: 0,
+                        rigth: 0,
+                        width: '100%',
+                        height: '100%',
+                        backgroundColor: 'lightgray',
+                    }}
+                ></div>
+            )}
+        </>
+    );
+}
