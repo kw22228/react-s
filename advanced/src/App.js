6d879cdbcd3df030eebf6345a85fa3fb448c31ff
kw22228 <hot5521@naver.com>
2021-11-15 10:56:33 +0900

    수정

--- advanced/src/App.js
+++ advanced/src/App.js
@@ -12,7 +12,6 @@ function App() {
         <div className="App">
             <Input />
             <br />
-            <br />
             <Button />
             {/* <Reducer />
             <State /> */}
