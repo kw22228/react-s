a7071baa8d0b354b84bc09f5829e8a2602b16f89
kw22228 <hot5521@naver.com>
2021-11-24 20:13:29 +0900

    수정

--- library/moment/src/App.js
+++ library/moment/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import MomentExample from './component/Moment/MomentExample';
-
+// import MomentExample from './component/Moment/MomentExample';
+// import DayjsExample from './component/Dayjs/DayjsExample';
+import DayFnsExample from './component/Datefns/DayFnsExample';
 function App() {
     return (
         <div className="App">
-            <MomentExample />
+            <DayFnsExample />
         </div>
     );
 }
