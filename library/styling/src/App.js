bd608ef85cb5ba27f289f297851103332ae0d456
kw22228 <hot5521@naver.com>
2021-11-24 23:29:43 +0900

    styling library

---
+++ library/styling/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StyledComponentsExample from './Components/StyledComponents/StyledComponentsExample';
+
+function App() {
+    return (
+        <div className="App">
+            <StyledComponentsExample />
+        </div>
+    );
+}
+
+export default App;
