2cf3d418be5046bdb9c1c62b49e7b25e640fd2a8
kw22228 <hot5521@naver.com>
2021-12-01 23:08:30 +0900

    UI INDEX

---
+++ library/ui/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+// Webpack CSS import
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+import OnsenuiExample from './Components/Onsenui/OnsenuiExample';
+
+function App() {
+    return <OnsenuiExample />;
+}
+
+export default App;
