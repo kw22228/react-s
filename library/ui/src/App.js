c5440681c40b1de39ee4bca3799a45f068760b87
kw22228 <hot5521@naver.com>
2021-12-02 17:41:14 +0900

    antdesign 추가

--- library/ui/src/App.js
+++ library/ui/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-
 // Webpack CSS import
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.css';
-import OnsenuiExample from './Components/Onsenui/OnsenuiExample';
+// import 'onsenui/css/onsenui.css';
+// import 'onsenui/css/onsen-css-components.css';
+// import OnsenuiExample from './Components/Onsenui/OnsenuiExample';
+
+import 'antd/dist/antd.css';
+import AntDesignExample from './Components/AntDesign/AntDesignExample';
 
 function App() {
-    return <OnsenuiExample />;
+    // return <OnsenuiExample />;
+    return <AntDesignExample />;
 }
 
 export default App;
