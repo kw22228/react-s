b1b44b3f5c707bb9ca0192172f2d1e95f2699210
kw22228 <hot5521@naver.com>
2021-12-03 19:55:56 +0900

    수정

--- library/ui/src/Components/AntDesign/AntDesignExample.jsx
+++ library/ui/src/Components/AntDesign/AntDesignExample.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import GetStart from './GetStart';
-import IconBtExample from './IconBtExample';
-import Typhography from './Typhography';
+import ModalExample from './ModalExample';
+// import FormExample from './FormExample';
+// import LayoutExample from './LayoutExample';
+import TableExample from './TableExample';
+// import GetStart from './GetStart';
+// import IconBtExample from './IconBtExample';
+// import Typhography from './Typhography';
 
 export default function AntDesignExample() {
     return (
         <>
-            <GetStart />
+            {/* <GetStart />
             <IconBtExample />
             <hr />
-            <Typhography />
+            <Typhography /> */}
+            {/* <LayoutExample /> */}
+            {/* <FormExample /> */}
+            <TableExample />
+            <hr />
+            <ModalExample />
         </>
     );
 }
