40ccba0ac633fb2c73d6cfb726cd33f5cde224e9
kw22228 <hot5521@naver.com>
2021-12-07 18:04:09 +0900

    React Bootstrap

--- library/ui/src/App.js
+++ library/ui/src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
-// Webpack CSS import
+
+// OnsenUI
 // import 'onsenui/css/onsenui.css';
 // import 'onsenui/css/onsen-css-components.css';
 // import OnsenuiExample from './Components/Onsenui/OnsenuiExample';
 
+//Antdesign
 // import 'antd/dist/antd.css';
 // import AntDesignExample from './Components/AntDesign/AntDesignExample';
 
-import 'semantic-ui-css/semantic.min.css';
-import ReactSemanticUIExample from './Components/SemanticUI/ReactSemanticUIExample';
+//Semantic UI
+// import 'semantic-ui-css/semantic.min.css';
+// import ReactSemanticUIExample from './Components/SemanticUI/ReactSemanticUIExample';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactBootstrap from './Components/BootstrapReact/ReactBootstrap';
 
 function App() {
     // return <OnsenuiExample />;
     // return <AntDesignExample />;
-    return <ReactSemanticUIExample />;
+    // return <ReactSemanticUIExample />;
+    return <ReactBootstrap />;
 }
 
 export default App;
