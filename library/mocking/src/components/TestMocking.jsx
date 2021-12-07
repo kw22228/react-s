6f312b6f90f1c2542d043c61ce187bf73753a97d
kw22228 <hot5521@naver.com>
2021-12-07 18:04:25 +0900

    mocking test

---
+++ library/mocking/src/components/TestMocking.jsx
@@ -0,0 +1,62 @@
+import { response } from 'msw';
+import React, { useState } from 'react';
+
+const Item = ({ name, age }) => {
+    return (
+        <li>
+            name : {name} / age : {age}
+        </li>
+    );
+};
+const url =
+    'https://raw.githubusercontent.com/techoi/raw-data-api/main/simple-api.json';
+
+export default function TestMocking() {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    const handleClick = () => {
+        fetch(url)
+            .then(res => {
+                return res.json();
+            })
+            .then(json => {
+                setData(json.data);
+            })
+            .catch(error => {
+                setError(error);
+            });
+    };
+    const handleClick2 = () => {
+        fetch('./login')
+            .then(res => {
+                return res.json();
+            })
+            .then(json => {
+                console.log(json);
+            })
+            .catch(error => {
+                setError(error);
+            });
+    };
+    if (error) {
+        return <p>{error}</p>;
+    }
+    return (
+        <div>
+            <button onClick={handleClick}>데이터 가져오기</button>
+            <button onClick={handleClick2}>데이터 가져오기2</button>
+            {data && (
+                <ul>
+                    {data.people.map(person => (
+                        <Item
+                            key={`${person.name}-${person.age}`}
+                            name={person.name}
+                            age={person.age}
+                        />
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
