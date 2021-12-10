95d858b54a51767bc716d4082ba23f781a1b0a3e
kw22228 <hot5521@naver.com>
2021-12-10 18:03:13 +0900

    data moking queryParams, patching, error

--- library/mocking/src/mocks/handler.js
+++ library/mocking/src/mocks/handler.js
@@ -11,39 +11,29 @@ export const handler = [
         );
     }),
     rest.get(
-        'https://raw.githubusercontent.com/techoi/raw-data-api/main/simple-api.json',
+        'https://raw.githubusercontent.com/techoi/raw-data-api/main/simple-api.json?id=react',
         async (req, res, ctx) => {
+            const id = req.url.searchParams.get('id');
+
+            const originalResponse = await ctx.fetch(req);
+            const originalResponseData = await originalResponse.json();
+
             return res(
+                ctx.status(403),
                 ctx.json({
-                    data: {
-                        people: [
-                            {
-                                name: 'jimmy',
-                                age: 133335,
-                            },
-                            {
-                                name: 'timmy',
-                                age: 13,
-                            },
-                            {
-                                name: 'cindy',
-                                age: 15,
-                            },
-                            {
-                                name: 'judy',
-                                age: 25,
-                            },
-                            {
-                                name: 'marry',
-                                age: 64,
-                            },
-                            {
-                                name: 'tommy',
-                                age: 109,
-                            },
-                        ],
-                    },
+                    errorMessage: 'Data not found',
                 })
+                // ctx.json({
+                //     data: {
+                //         people: [
+                //             ...originalResponseData.data.people, //원래 가져오던 서버에서 가져옴
+                //             {
+                //                 name: 'id',
+                //                 age: 133335,
+                //             },
+                //         ],
+                //     },
+                // })
             );
         }
     ),
