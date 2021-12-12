89ad21dc61ca75abaa8448907e6ec3d7129a5957
kw22228 <hot5521@naver.com>
2021-12-12 23:40:40 +0900

    데이터 모킹

---
+++ library/redux/src/mocks/handler.js
@@ -0,0 +1,40 @@
+import { rest } from 'msw';
+
+export const handler = [
+    rest.get('/login', async (req, res, ctx) => {
+        return res(
+            ctx.json({
+                id: 'ffffffffff',
+                firstName: 'jaewon',
+                lastName: 'Kim',
+            })
+        );
+    }),
+    rest.get(
+        'https://raw.githubusercontent.com/techoi/raw-data-api/main/simple-api.json?id=react',
+        async (req, res, ctx) => {
+            const id = req.url.searchParams.get('id');
+
+            const originalResponse = await ctx.fetch(req);
+            const originalResponseData = await originalResponse.json();
+
+            return res(
+                ctx.status(403),
+                ctx.json({
+                    errorMessage: 'Data not found',
+                })
+                // ctx.json({
+                //     data: {
+                //         people: [
+                //             ...originalResponseData.data.people, //원래 가져오던 서버에서 가져옴
+                //             {
+                //                 name: 'id',
+                //                 age: 133335,
+                //             },
+                //         ],
+                //     },
+                // })
+            );
+        }
+    ),
+];
