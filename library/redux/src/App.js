e8612a36812629f713d208b01cf76218147246bb
kw22228 <hot5521@naver.com>
2021-12-12 23:40:57 +0900

    수정

---
+++ library/redux/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Counter2 from './components/Counter2';
+import Counter from './features/counter/Counter';
+function App() {
+    return (
+        <div className="App">
+            <Counter />
+            <br />
+            <br />
+            <br />
+            <br />
+            <Counter2 />
+        </div>
+    );
+}
+
+export default App;
