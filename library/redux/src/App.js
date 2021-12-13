156dbc42f2330d412ef6015a5d654714f1878337
kw22228 <hot5521@naver.com>
2021-12-13 22:57:39 +0900

    mobx 추가

--- library/redux/src/App.js
+++ library/redux/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
+import { observableTodoStore } from './app/ObservableTodoStore';
+// import MobxExmaple from './components/MobxExmaple';
+import TodoList from './components/TodoList';
 // import Counter2 from './components/Counter2';
-import Counter from './features/counter/Counter';
+// import Counter from './features/counter/Counter';
 function App() {
     return (
         <div className="App">
-            <Counter />
+            {/* <Counter /> */}
             {/* <br />
             <br />
             <br />
             <br />
             <Counter2 /> */}
+
+            {/* <MobxExmaple /> */}
+            <TodoList store={observableTodoStore} />
         </div>
     );
 }
