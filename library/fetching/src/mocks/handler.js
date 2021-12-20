0a79266f30c9a26b1dae6c2f0b6fdf0f27c2fab6
kw22228 <hot5521@naver.com>
2021-12-20 18:01:22 +0900

    모킹 데이터 수정

--- library/fetching/src/mocks/handler.js
+++ library/fetching/src/mocks/handler.js
@@ -1,13 +1,41 @@
 import { rest } from 'msw';
 
 export const handler = [
+    rest.get('http://localhost:3000/api/users', async (req, res, ctx) => {
+        const pageIndex = req.url.searchParams.get['page'];
+        return res(
+            ctx.json([
+                {
+                    id: `1 ${pageIndex}`,
+                    name: `jaewon 1 ${pageIndex}`,
+                },
+                {
+                    id: `2 ${pageIndex}`,
+                    name: `jaewon 2 ${pageIndex}`,
+                },
+                {
+                    id: `3 ${pageIndex}`,
+                    name: `jaewon 3 ${pageIndex}`,
+                },
+                {
+                    id: `4 ${pageIndex}`,
+                    name: `jaewon 4 ${pageIndex}`,
+                },
+                {
+                    id: `5 ${pageIndex}`,
+                    name: `jaewon 5 ${pageIndex}`,
+                },
+            ])
+        );
+        // return res(ctx.status(400));
+    }),
     rest.get(
         'http://localhost:3000/api/user/:userId',
         async (req, res, ctx) => {
             const { userId } = req.params;
             return res(
                 ctx.json({
-                    name: `JAEWON (${userId})`,
+                    name: `jaewon (${userId})`,
                 })
             );
             // return res(ctx.status(400));
