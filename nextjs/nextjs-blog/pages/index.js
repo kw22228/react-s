bd28b0244aa6ea1d7fafd1189c5fc1118a62820a
kw22228 <hot5521@naver.com>
2022-01-04 21:58:18 +0900

    dynamic route polishing index

--- nextjs/nextjs-blog/pages/index.js
+++ nextjs/nextjs-blog/pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Date from '../components/date';
 import Layout, { siteTitle } from '../components/layout';
 import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 
 export default function Home({ allPostsData }) {
-    console.log(allPostsData);
     return (
         <Layout home>
             <Head>
@@ -33,11 +33,14 @@ export default function Home({ allPostsData }) {
                     {allPostsData.map(({ id, date, title }) => {
                         return (
                             <li className={utilStyles.listItem} key={id}>
-                                {title}
+                                <Link href={`/posts/${id}`}>
+                                    <a>{title}</a>
+                                </Link>
                                 <br />
-                                {id}
-                                <br />
-                                {date}
+                                <small className={utilStyles.lightText}>
+                                    <Date dateString={date} />
+                                </small>
+                                <hr />
                             </li>
                         );
                     })}
